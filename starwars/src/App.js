4c5f3c14b5bf944c1dc41a4a8d6670633af5d354
Jacob Bryan <jtb36688@gmail.com>
2019-01-11 18:04:55 -0500

    Testing Commit

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -44,7 +44,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <h1 className="Header">Reactt Wars</h1>
         <div className="MainContent">
           <div className='listdiv'>
           <CharList
